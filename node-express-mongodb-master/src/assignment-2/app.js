2b25ae0cf6ea2ae6cacbea854f020390e91de5ef
Richard Roy <richard_roy@hotmail.com>
2019-11-08 23:44:13 -0700

    Added mongoose and dotenv

--- node-express-mongodb-master/src/assignment-2/app.js
+++ node-express-mongodb-master/src/assignment-2/app.js
@@ -1,3 +1,7 @@
+//require dotenv
+const dotenv = require('dotenv');
+//envoke dotenv
+dotenv.config();
 //require express
 const express = require('express');
 //require path module to deal with file paths
@@ -8,16 +12,27 @@ const pageInfo = require('./pageInfo');
 const middleware = require('./middleware');
 //envoke express
 const app = express();
+//require mongoose
+const mongoose = require('mongoose');
 
 //Made and included favicon
 const favicon = require('express-favicon');
 app.use(favicon(__dirname + '/assets/img/gem.ico/favicon.ico'));
 
+//need the mongoose code higher up, it is used to connect to db (works!)
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true,useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DB Connected!!!');
+});
 
 //exclude .ejs from url
 app.set('view engine','ejs'); 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 
 //use app.get to render the .ejs
@@ -33,18 +48,45 @@ app.get('/', function(request, response){
 //evoking middleware module to log name:email in log.txt when form is submitted
 app.use(middleware)
 
+//assignment3
+app.post('/submit', function(req,res){ 
+  const name = req.body.name; 
+  const email =req.body.email; 
+
+  const data = { 
+      "name": name, 
+      "email": email
+  } 
+
+db.collection('details').insertOne(data, (err, collection) => { 
+      if (err) throw err; 
+      console.log("Record inserted Successfully"); 
+            
+  }); 
+        
+  res.render('submit', {data: req.body})
+}) 
+
+
+app.get('/',function(req,res){ 
+  res.set({ 
+    'Access-control-Allow-Origin': '*'
+    }); 
+  return res.redirect('index'); 
+  })
+
 
 //use app.post to redirect users to /submit upon submitting form in footer
-app.post('/submit', function(request,response) {
-  response.render('submit', {data: request.body})
-  console.log('updated')
-});
+// app.post('/submit', function(request,response) {
+//   response.render('submit', {data: request.body})
+//   console.log('updated')
+// });
 
 //set assets as a static folder so it renders all the files within it
 app.use(express.static(path.join(__dirname, 'assets')));
 
 //create a variable with a PORT number. (process.env.port looks at the environment variables for PORT or PORT 3000)
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 //listen on port 3000, add a console.log callback to verify port is running
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
